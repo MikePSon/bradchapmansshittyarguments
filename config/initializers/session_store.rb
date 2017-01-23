dc802861b60f1cc9ed5bf8549b2e0210bc18cef4
Mike Patterson <info@mikeypatterson.com>
2017-01-23 16:53:06 -0500

    initial commit (RailsBricks 3.2.1)

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bradchapmansshittyarguments::Application.config.session_store :cookie_store, key: '_bradchapmansshittyarguments_session'
